85fe5999e9080ba6717095cc7e063c02db7ff2b9
unknown <Brennan.artzt@gmail.com>
2017-09-13 18:19:42 -0500

    bug fix 3 no repeats

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	var searchQuery;
+    var currentQuery;
 	var currentPlay;
 	var topics= ["Andy Warhol", "Diego Rivera", "Francisco de Goya", "Frida Kahl", "Georges Seurat", "Henri Rousseau", "Jack Vettriano", "Jackson Pollock", "Joan Mir\xF3", "Leonardo da Vinci", "Marc Chagall", "Mary Cassatt", "Norman Rockwell", "Pablo Picasso", "Paul C\xE9zanne", "Paul Gauguin", "Pierre Auguste Renoir", "Wassily Kandinsky", "Winslow Homer", "\xC9douard Manet"];
 	renderButtons();
@@ -25,9 +26,10 @@ $(document).ready(function() {
         }
     });
     $(document).on("click", ".Generate-Gallery", function() {
-    	if(searchQuery===$(this).data("Artist"))
+    	if(currentQuery===$(this).data("Artist"))
     		return;
     	searchQuery=$(this).data("Artist");
+        currentQuery=$(this).data("Artist");
     	currentPlay=undefined;
     	$("#Gallery").empty();
     	$("#Gallery").append("<h1 class='col-md-12'>"+searchQuery+"<h1>");
@@ -40,6 +42,7 @@ $(document).ready(function() {
       		url: queryURL,
      		 method: 'GET'
     	}).done(function(response) {
+    		//iterates through placing the images
     		for (var i = 0; i < response.data.length; i++) {
     			var entry=$("<div>");
     			var img= $("<img>");
@@ -53,19 +56,24 @@ $(document).ready(function() {
     		}	
     	});
     });
+    //click function on gif
     $(document).on("click", ".Play", function() {
+    	//stops current running gif
     	if(currentPlay!=undefined)
     	{
     		$(currentPlay).attr("src", $(currentPlay).data("giphyData").images.fixed_width_still.url);
     	}
+    	//checks to see if click was on currently running gif and removes its info
     	if(currentPlay===this)
     	{
     		currentPlay=undefined;
     		return;
     	}
+    	//starts the new playing gif
     	currentPlay=this;
     	$(this).attr("src", $(this).data("giphyData").images.fixed_width.url);
     });
+    //preview gif by hovering
     $(document).on("mouseenter", ".Play", function() {
     	if(currentPlay===this)
     	{
@@ -73,6 +81,7 @@ $(document).ready(function() {
     	}
     	$(this).attr("src", $(this).data("giphyData").images.fixed_width.url);
     });
+    //ends preview 
     $(document).on("mouseleave", ".Play", function() {
     	if(currentPlay===this)
     	{
